5411da754503a308040afa3e7eda6b862eabe849
CBSanta <catalin.beniamin@babel.es>
2018-06-07 23:21:09 +0200

    composer basic layout

--- frontend/src/app/composer/composer.component.ts
+++ frontend/src/app/composer/composer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-composer',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComposerComponent implements OnInit {
 
+  public previewDimensions = {};
+  public paperDimensions = {};
+
   constructor() { }
 
+  public htmlProps;
+
   ngOnInit() {
+
+    this.previewDimensions = {
+      height: 0,
+      width: 0
+    }
+
+    const baseDimension = window.innerWidth * 0.55;
+
+    this.paperDimensions = {
+      width: baseDimension,
+      height: baseDimension * Math.sqrt(2)
+    }
+
+    this.onResize(null);
+
+  }
+
+  public onResize(event) {
+    this.previewDimensions = {
+      height: window.innerHeight
+        - document.querySelector("app-header div").clientHeight
+        - document.querySelector("app-footer div").clientHeight
+        - document.querySelector("div.b-window-header").clientHeight,
+      width: window.innerWidth
+        - document.querySelector("div.section-left").clientWidth * 2
+    }
   }
 
 }
