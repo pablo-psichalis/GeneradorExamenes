eca7fe41cf336f95da2e1919bc896372e4216572
pablo-psichalis <pablo.psrz@gmail.com>
2018-06-13 18:15:53 +0200

    Fix No 'Access-Control-Allow-Origin'

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
