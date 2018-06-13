744c25d30e14be08c095c44ee3a27d94fc124fc7
CBSanta <catalin.beniamin@babel.es>
2018-06-13 21:17:05 +0200

    logged in home username

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../services/shared.service';
+import { UsersService } from '../services/users.service';
+import { ErrorService } from '../services/error.service';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +13,22 @@ export class HomeComponent implements OnInit {
   public isLogged: boolean;
   public isAdmin: boolean;
 
+  public loggedUser: {
+    _id: string,
+    username: string
+  };
+
   public pageHeight: number;
 
   constructor(
-    private sharedService: SharedService
+    private sharedService: SharedService,
+    private usersSerivice: UsersService,
+    private errorService: ErrorService,
   ) { }
 
   ngOnInit() {
+    this.pageHeight = 0;
+    this.onResize(null);
     this.sharedService.loginEmitted.subscribe(data => {
       if (data !== 'NOT_INIT') {
         this.sharedService.emitStatus('UNLOADED');
@@ -29,9 +40,16 @@ export class HomeComponent implements OnInit {
   }
 
   private loadComponent() {
-    this.pageHeight = 0;
-    this.onResize(null);
-    this.sharedService.emitStatus('LOADED');
+    this.loggedUser = {
+      _id: '',
+      username: ''
+    };
+    this.usersSerivice.getUser()
+      .then(res => {
+        this.loggedUser = res;
+        this.sharedService.emitStatus('LOADED');
+      })
+      .catch(err => this.errorService.throwError(err, this));
   }
 
   public onResize(event) {
