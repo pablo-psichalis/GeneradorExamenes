70c3220cf78cf55e0d05703ccc8a5c0735a1226a
CBSanta <catalin.beniamin@babel.es>
2018-06-22 00:42:32 +0200

    collections editing overlay working, todo: collections service integration

--- frontend/src/app/collections/collections.component.ts
+++ frontend/src/app/collections/collections.component.ts
@@ -7,6 +7,7 @@ import { CollectionsService } from '../services/collections.service';
   templateUrl: './collections.component.html',
   styleUrls: ['./collections.component.css']
 })
+// tslint:disable:max-line-length
 export class CollectionsComponent implements OnInit {
 
   public isLogged: boolean;
@@ -16,9 +17,17 @@ export class CollectionsComponent implements OnInit {
   public previewWidth: number;
 
   public oCollections: Array<any>;
+  public quillModules: {};
+  public quillStyle: {};
+
   public selectedId: number;
   public deletingQId: number;
   public deletingCId: number;
+  public editingQId: number;
+  public editingCId: number;
+  public editingOId: number;
+
+  public editingQ: any;
 
   constructor(
     private sharedService: SharedService,
@@ -26,11 +35,42 @@ export class CollectionsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.quillModules = {
+      toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+        [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+        [{ 'align': [] }],
+        [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [false, 5, 4, 3, 2, 1] }],
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        ['clean'],                                         // remove formatting button
+        ['image']                         // link and image, video
+      ]
+    };
+    this.quillStyle = {
+      'height': 'auto',
+      'min-height': '5rem'
+    };
+
     this.pageHeight = 0;
     this.previewWidth = 0;
+
     this.selectedId = -1;
     this.deletingQId = -1;
     this.deletingCId = -1;
+    this.editingQId = -1;
+    this.editingCId = -1;
+    this.editingOId = -1;
+
+    this.editingQ = {};
+
     this.sharedService.loginEmitted.subscribe(data => {
       if (data !== 'NOT_INIT') {
         this.sharedService.emitStatus('UNLOADED');
@@ -55,6 +95,29 @@ export class CollectionsComponent implements OnInit {
       - document.querySelector('div.b-window-header').clientHeight;
     this.previewWidth = window.innerWidth
       - document.querySelector('div.menu-left').clientWidth;
+  }
+
+  public editQuestion(id, status: boolean = true) {
+    if (status) {
+      this.editingQId = id;
+      this.editingQ = JSON.parse(JSON.stringify(this.oCollections[this.selectedId].questions[this.editingQId]));
+    } else {
+      this.editingQId = -1;
+      this.editingOId = -1;
+      this.editingQ = {
+        statement: '',
+        difficulty: 0,
+        type: '',
+        options: [],
+        correct_option: 0,
+        solution: ''
+      };
+    }
+  }
+
+  public saveQuestion() {
+    this.oCollections[this.selectedId].questions[this.editingQId] = JSON.parse(JSON.stringify(this.editingQ));
+    this.editQuestion(null, false);
   }
 
   public deleteQuestion(id) {
--- frontend/src/app/collections/collections.module.ts
+++ frontend/src/app/collections/collections.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CollectionsComponent } from './collections.component';
 import { QuillModule } from 'ngx-quill';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     QuillModule,
+    FormsModule,
   ],
   declarations: [CollectionsComponent]
 })
